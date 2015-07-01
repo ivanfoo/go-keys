c4f9b183a67abf5d25ec4f30b79ecc1857ea4bf5
ivanfoo <sanfru.ivan@gmail.com>
2015-07-01 17:13:38 +0200

    Initial commit

---
+++ qiis.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "crypto/x509"
+    "crypto/rsa"
+    "crypto/rand"
+    "encoding/pem"
+)
+
+func main() {
+
+    privateKey, err := rsa.GenerateKey(rand.Reader, 2014)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    privateKeyDer := x509.MarshalPKCS1PrivateKey(privateKey)
+
+    privateKeyBlock := pem.Block{
+        Type:    "RSA PRIVATE KEY",
+        Headers: nil,
+        Bytes:   privateKeyDer,
+    }
+
+    privateKeyPem := string(pem.EncodeToMemory(&privateKeyBlock))
+
+    publicKey := privateKey.PublicKey
+
+    publicKeyDer, err := x509.MarshalPKIXPublicKey(&publicKey)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    publicKeyBlock := pem.Block{
+        Type:    "PUBLIC KEY",
+        Headers: nil,
+        Bytes:   publicKeyDer,
+    }
+
+    publicKeyPem := string(pem.EncodeToMemory(&publicKeyBlock))
+
+    fmt.Println(privateKeyPem)
+    fmt.Println(publicKeyPem)   
+}
+
